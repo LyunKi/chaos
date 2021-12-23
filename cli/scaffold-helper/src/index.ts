3e83b939f500808449e030b2012c447cfbae406d
l00624481 <longyun13@huawei.com>
2021-12-23 21:11:15 +0800

    chore: initial commit

---
+++ cli/scaffold-helper/src/index.ts
@@ -0,0 +1,11 @@
+import { Command } from 'commander';
+import pkg from '../package.json'
+
+const program = new Command();
+
+program
+    .version(pkg.version)
+    .description(pkg.description)
+    .command('gc [type]','generate your configs, if type not specified, all configs will be generated',{isDefault:true,executableFile:'scaffold-helper-gc'})
+    .showHelpAfterError()
+    .parse(process.argv)
---
+++ cli/scaffold-helper/src/scaffold-helper-gc.ts
@@ -0,0 +1,35 @@
+import { Command } from 'commander';
+import path from 'path';
+
+const program = new Command();
+
+enum ConfigType {
+    FORMAT = 'FORMAT'
+}
+
+function generatePrettierConfig() {
+}
+
+interface GeneratorOptions {
+    projectPath?: string
+}
+
+async function generate(options:GeneratorOptions,command:Command) {
+    const {projectPath} = options;
+    const root = projectPath ?? process.cwd();
+    switch('FORMAT') {
+        case ConfigType.FORMAT : {
+            generatePrettierConfig();
+            break;
+        }
+        default: {
+            // generate all configs for user
+        }
+    }
+}
+
+program
+.option('-p, --project [projectPath]','project root path to add configs')
+.action(generate)
+.showHelpAfterError()
+.parseAsync(process.argv)
