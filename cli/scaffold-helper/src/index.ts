5ff63a377e491998ec5f912f39fb4a51a30e238d
l00624481 <longyun13@huawei.com>
2022-01-11 17:37:25 +0800

    chore(cloud-dragon): add format config

--- cli/scaffold-helper/src/index.ts
+++ cli/scaffold-helper/src/index.ts
@@ -1,11 +1,15 @@
-import { Command } from 'commander';
+import { Command } from 'commander'
 import pkg from '../package.json'
 
-const program = new Command();
+const program = new Command()
 
 program
-    .version(pkg.version)
-    .description(pkg.description)
-    .command('gc [type]','generate your configs, if type not specified, all configs will be generated',{isDefault:true,executableFile:'scaffold-helper-gc'})
-    .showHelpAfterError()
-    .parse(process.argv)
+  .version(pkg.version)
+  .description(pkg.description)
+  .command(
+    'gc [type]',
+    'generate your configs, if type not specified, all configs will be generated',
+    { isDefault: true, executableFile: 'scaffold-helper-gc' },
+  )
+  .showHelpAfterError()
+  .parse(process.argv)
