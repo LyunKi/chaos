9f15e387d64d35af8de981436a8f1b1a875a9acf
l00624481 <longyun13@huawei.com>
2022-02-24 20:33:17 +0800

    chore: make native app runnable

--- native/cas-client/src/hooks/useCachedResources.ts
+++ native/cas-client/src/hooks/useCachedResources.ts
@@ -15,7 +15,8 @@ export default function useCachedResources() {
         // Load fonts
         await Font.loadAsync({
           ...FontAwesome.font,
-          'space-mono': require('../../assets/fonts/SpaceMono-Regular.ttf'),
+          // TODO: choose a beautiful font to add
+          // 'space-mono': require('../../assets/fonts/SpaceMono-Regular.ttf'),
         })
       } catch (e) {
         // We might want to provide this error information to an error reporting service
