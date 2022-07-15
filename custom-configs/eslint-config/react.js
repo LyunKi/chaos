49824ec2f19a11e17f09ba4072b7f2d52587cc39
l00624481 <longyun13@huawei.com>
2022-07-15 15:46:44 +0800

    feat: 增加 import 相关规则

--- custom-configs/eslint-config/react.js
+++ custom-configs/eslint-config/react.js
@@ -1,3 +1,39 @@
 module.exports = {
-  extends: ['react-app'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  extends: [
+    'react-app',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  plugins: ['autofix', 'unused-imports', 'import'],
+  rules: {
+    'autofix/no-debugger': 'error',
+    'autofix/no-console': 'error',
+    'autofix/no-plusplus': 'error',
+    'import/no-unresolved': 'error',
+    'import/order': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'import/no-named-as-default-member': 'off',
+    'import/namespace': 'off',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
 }
