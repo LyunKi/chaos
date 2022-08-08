addac2c3bc5c875e4b2fc9508df422192951ae2b
l00624481 <longyun13@huawei.com>
2022-08-08 14:36:07 +0800

    feat(eslint-config): remove prefer-default-export rule

--- custom-configs/eslint-config/index.js
+++ custom-configs/eslint-config/index.js
@@ -1,18 +1,19 @@
 module.exports = {
-    extends: [
-        'airbnb-base',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'prettier/prettier',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {
-        'no-shadow': 'off',
-        '@typescript-eslint/no-shadow': ['error'],
-    },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'import/prefer-default-export': 'off',
+  },
 }
