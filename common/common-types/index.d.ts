249dc7421560a70125eb08090149720fd17cf5b2
l00624481 <longyun13@huawei.com>
2022-08-29 17:19:42 +0800

    feat(common-types): add Prefix type

--- common/common-types/index.d.ts
+++ common/common-types/index.d.ts
@@ -3,3 +3,5 @@ export type KV<T = any> = Record<string, T>
 export type NestedString = {
   [key: string]: string | NestedString
 }
+
+export type Prefix<K extends string, T extends string = string> = `${K}${T}`
