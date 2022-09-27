9e25e568164163576390b01b08df5eb679571335
l00624481 <longyun13@huawei.com>
2022-09-27 15:15:13 +0800

    feat(react-utils): add combineRefs method

---
+++ common/react-utils/src/refs/index.ts
@@ -0,0 +1,17 @@
+import { MutableRefObject, RefCallback } from 'react'
+
+export type SupportedRef<T> = RefCallback<T> | MutableRefObject<T>
+
+function attachRef<T>(ref: SupportedRef<T>, node: T) {
+  if (typeof ref === 'function') {
+    ref(node)
+  } else if (typeof ref === 'object') {
+    ref.current = node
+  }
+}
+
+export function combineRefs<T>(...refs: SupportedRef<T>[]) {
+  return function binder(node: T) {
+    refs.filter((ref) => ref).forEach((ref) => attachRef(ref, node))
+  }
+}
