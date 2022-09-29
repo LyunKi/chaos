e54e88fed9a9a046131d165197958fb1b593aea8
l00624481 <longyun13@huawei.com>
2022-09-29 15:06:11 +0800

    chore: update dependencies

--- native/cas-client/src/App.tsx
+++ native/cas-client/src/App.tsx
@@ -6,30 +6,29 @@ import AppLoading from 'expo-app-loading'
 import { ThemeProvider } from 'styled-components/native'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
 
+import { useColorScheme } from 'react-native'
 import useCachedResources from './common/utils/hooks/useCachedResources'
 import Navigation from './navigation'
 import { CountriesIconsPack } from './components'
 
 export default function App() {
   const isLoadingComplete = useCachedResources()
-  // const colorScheme = useColorScheme()
-  const colorScheme = 'dark'
-  // const colorScheme = 'light'
+  const colorScheme = useColorScheme() ?? 'light'
+  // const theme = `cloud-${colorScheme}`;
   const theme = eva[colorScheme]
 
   if (!isLoadingComplete) {
     return <AppLoading />
-  } else {
-    return (
-      <ApplicationProvider {...eva} theme={theme}>
-        <IconRegistry icons={[EvaIconsPack, CountriesIconsPack]} />
-        <SafeAreaProvider>
-          <ThemeProvider theme={theme}>
-            <Navigation colorScheme={colorScheme} />
-          </ThemeProvider>
-          <StatusBar />
-        </SafeAreaProvider>
-      </ApplicationProvider>
-    )
   }
+  return (
+    <ApplicationProvider {...eva} theme={theme}>
+      <IconRegistry icons={[EvaIconsPack, CountriesIconsPack]} />
+      <SafeAreaProvider>
+        <ThemeProvider theme={theme}>
+          <Navigation colorScheme={colorScheme} />
+        </ThemeProvider>
+        <StatusBar />
+      </SafeAreaProvider>
+    </ApplicationProvider>
+  )
 }
