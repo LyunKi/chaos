1a582b05aa7854ea5355e58b17dc85ff8e5c9f5c
l00624481 <longyun13@huawei.com>
2023-01-29 15:52:25 +0800

    feat: add CommonValidator util

--- common/utils/src/validation.ts
+++ common/utils/src/validation.ts
@@ -59,3 +59,16 @@ export class ValidationSchema {
     )
   }
 }
+
+const EMAIL_REGEX =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+export const enum ValidateType {
+  EMAIL,
+}
+
+export class CommonValidator {
+  public static validateEmail(email: string) {
+    return EMAIL_REGEX.test(email)
+  }
+}
