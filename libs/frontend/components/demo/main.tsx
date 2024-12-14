c0c745e5798add541f8c0db934c8a77da176de21
l00624481 <longyun13@huawei.com>
2024-12-14 17:44:46 +0800

    chore: add biome as formatter and linter

--- libs/frontend/components/demo/main.tsx
+++ libs/frontend/components/demo/main.tsx
@@ -1,14 +1,20 @@
-import { createRoot } from 'react-dom/client';
-import { Button } from '@/index';
-import  '@/index.css';
+import { createRoot } from "react-dom/client";
+import { Button } from "@/index";
+import "@/index.css";
 function App() {
-  return (
-    <div style={{width:'100vw', height:'100vh', display:'flex', justifyContent:'center',alignItems: 'center'}}>
-      <Button variant={'destructive'} >
-        Shadcn Button
-      </Button>
-    </div>
-  );
+	return (
+		<div
+			style={{
+				width: "100vw",
+				height: "100vh",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Button variant={"destructive"}>Shadcn Button</Button>
+		</div>
+	);
 }
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
--- libs/frontend/components/vite.config.ts
+++ libs/frontend/components/vite.config.ts
@@ -1,34 +1,32 @@
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
+import { defineConfig } from "vite";
 
 const globals = {
-  'react-dom': 'ReactDom',
-  react: 'React',
+	"react-dom": "ReactDom",
+	react: "React",
 };
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  build: {
-    lib: {
-      name: 'CloudDesignComponents',
-      entry: './src/index.ts',
-      fileName: 'index',
-    },
-    rollupOptions: {
-      output: {
-        globals,
-      },
-    },
-      },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    open: '/demo/index.html',
-  },
+	plugins: [react()],
+	build: {
+		lib: {
+			name: "CloudDesignComponents",
+			entry: "./src/index.ts",
+			fileName: "index",
+		},
+		rollupOptions: {
+			output: {
+				globals,
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		open: "/demo/index.html",
+	},
 });
